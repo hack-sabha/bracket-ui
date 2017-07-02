e8139eda56427d355a800d0d0754f800c941c1d6
Avinash Kolluavinashkollu@gmail.com <AvinashKollu@users.noreply.github.com>
2017-07-02 12:55:36 -0500

    Adding Angular Material to the Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -8,6 +9,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+
+//Material Design
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -20,8 +24,10 @@ import { TournamentService }          from './services/tournament.service';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
+        MaterialModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
